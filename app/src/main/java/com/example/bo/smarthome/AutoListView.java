9699d66791e68d8e013fb9dbface5cbdb757d707
Zhen <qu000010@algonquinlive.com>
2017-04-04 21:17:32 -0400

    Milestone 2

--- app/src/main/java/com/example/bo/smarthome/AutoListView.java
+++ app/src/main/java/com/example/bo/smarthome/AutoListView.java
@@ -1,6 +1,8 @@
 package com.example.bo.smarthome;
 
 import android.app.AlertDialog;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -10,40 +12,33 @@ import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
 public class AutoListView extends AppCompatActivity {
     Context ctx;
-    ListView listView;
-    EditText editText;
-    Spinner spinner;
-    Button addButton;
-    ArrayList<String> autoUnit = new ArrayList<String>();
+    ArrayList<String> autoArray = new ArrayList<String>();
     ArrayAdapter<String> listAdapter;
     protected static final String ACTIVITY_NAME = "AutoListView";
     protected AutoDatabaseHelper dbHelper;
+    public SQLiteDatabase db;
     protected Boolean isTablet;
     Cursor results;
-    //MessageFragment frag;
+    AutoTemperatureFragment frag;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,62 +49,93 @@ public class AutoListView extends AppCompatActivity {
         ctx = this;
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.auto_fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Snackbar.make(fab, "Welcome to Auto Settings", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
 
         ListView autoListView =(ListView)findViewById(R.id.auto_ListView);
 
-        String [] item = {"Auto Temp", "Auto Normal Light", "Auto GPS"};
-        ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(item));
-        listAdapter =new ArrayAdapter <> (this,R.layout.activity_auto_list_view_item,R.id.auto_AddedListView,arrayList);
+        String [] item = {"Auto Temperature", "Auto Lights", "Auto GPS", "Auto Radio Stations"};
+        autoArray = new ArrayList<>(Arrays.asList(item));
+        listAdapter =new ArrayAdapter <> (this,R.layout.activity_auto_list_view_item,R.id.auto_AddedListView,autoArray);
         autoListView.setAdapter(listAdapter);
 
-        addButton = (Button) findViewById(R.id.autoListViewAddButton);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder2 = new AlertDialog.Builder(ctx);
-                // Get the layout inflater
-                LayoutInflater inflater = getLayoutInflater();
-                final View v = inflater.inflate(R.layout.activity_auto_add_new_unit, null);
-                // Inflate and set the layout for the dialog
-                // Pass null as the parent view because its going in the dialog layout
-                builder2.setView(inflater.inflate(R.layout.activity_auto_add_new_unit, null))
-                        // Add action buttons
-                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int id) {
-                                Log.i("Yes", "Yes");
-                                Spinner spinner = (Spinner) v.findViewById(R.id.auto_AddNewUnitSpinner);
-                                Log.i("Text is:", spinner.getSelectedItem().toString());
-                                String newAutoUnit = spinner.getSelectedItem().toString();
-
-                                listAdapter.remove(newAutoUnit);
-                                listAdapter.add(newAutoUnit);
-                            }
-                        })
-                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                Log.i("No", "No");
-                            }
-                        });
-                builder2.setView(v);
-                builder2.create().show();
-            }
-        });
+        isTablet = (findViewById(R.id.auto_FragmentHolder)!=null);
+        dbHelper = new AutoDatabaseHelper(this);
+        db = dbHelper.getWritableDatabase();
+        results = db.query(false, AutoDatabaseHelper.DATABASE_NAME,
+                new String[] { AutoDatabaseHelper.KEY_ID, AutoDatabaseHelper.KEY_GPS_ENTRY, AutoDatabaseHelper.KEY_TEMPERATURE, AutoDatabaseHelper.KEY_NORMAL_SWITCH,
+                               AutoDatabaseHelper.KEY_HEAD_SWITCH, AutoDatabaseHelper.KEY_INSIDE_BRIGHTNESS, AutoDatabaseHelper.KEY_RADIO_VOLUME, AutoDatabaseHelper.KEY_RADIO_MUTE,
+                               AutoDatabaseHelper.KEY_RADIO_STATION_ONE, AutoDatabaseHelper.KEY_RADIO_STATION_TWO, AutoDatabaseHelper.KEY_RADIO_STATION_THREE, AutoDatabaseHelper.KEY_RADIO_STATION_FOUR,
+                               AutoDatabaseHelper.KEY_RADIO_STATION_FIVE, AutoDatabaseHelper.KEY_RADIO_STATION_SIX}, null, null, null, null, null, null);
+        results.moveToFirst();
 
         autoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("autoListView", "onItemClick: " + i + " " + l);
+                Bundle bun = new Bundle();
+                long id = results.getLong(results.getColumnIndex(AutoDatabaseHelper.KEY_ID));
+                String gpsEntry = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_GPS_ENTRY));
+                String temp = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_TEMPERATURE));
+                String normalSwitch = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_NORMAL_SWITCH));
+                String headSwitch = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_HEAD_SWITCH));
+                String insideBrightness = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_INSIDE_BRIGHTNESS));
+                String volumn = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_RADIO_VOLUME));
+                String mute = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_RADIO_MUTE));
+                String radiaostation1 = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_RADIO_STATION_ONE));
+                String radiaostation2 = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_RADIO_STATION_TWO));
+                String radiaostation3 = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_RADIO_STATION_THREE));
+                String radiaostation4 = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_RADIO_STATION_FOUR));
+                String radiaostation5 = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_RADIO_STATION_FIVE));
+                String radiaostation6 = results.getString(results.getColumnIndex(AutoDatabaseHelper.KEY_RADIO_STATION_SIX));
 
+                bun.putLong("id",id);
+                bun.putString("GPSEntry",gpsEntry);
+                bun.putString("TEmperature",temp);
+                bun.putString("NormalSwitch",normalSwitch);
+                bun.putString("HeadSwitch",headSwitch);
+                bun.putString("InsideBrightness",insideBrightness);
+                bun.putString("Volumn",volumn);
+                bun.putString("Mute",mute);
+                bun.putString("Station1",radiaostation1);
+                bun.putString("Station2",radiaostation2);
+                bun.putString("Station3",radiaostation3);
+                bun.putString("Station4",radiaostation4);
+                bun.putString("Station5",radiaostation5);
+                bun.putString("Station6",radiaostation6);
 
-                if (adapterView.getItemAtPosition(i).equals("Auto GPS")) {
+                if (adapterView.getItemAtPosition(i).equals("Auto Temperature")) {
+                    if (isTablet) {
+                        String unit = "Auto Temperature";
+                        bun.putString("AutoUnit",unit);
+                        frag = new AutoTemperatureFragment(AutoListView.this);
+                        frag.setArguments(bun);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.auto_FragmentHolder, frag).commit();
+                    }else {
+                        Intent intnt = new Intent(AutoListView.this, AutoItemDetails.class);
+                        intnt.putExtra("id", id);
+                        intnt.putExtra("Temperature", temp);//pass the Database ID and message to next activity
+                        startActivityForResult(intnt, 5); //go to view fragment details
+                    }
+
+                    NotificationCompat.Builder mBuilder =
+                            new NotificationCompat.Builder(ctx)
+                                    .setSmallIcon(R.drawable.car1)
+                                    .setContentTitle("Notification")
+                                    .setContentText("Auto Temperature Setting");
+
+                    //where to go if clicked
+                    Intent resultIntent = new Intent(ctx, AutoItemDetails.class);
+                    PendingIntent resultPendingIntent = PendingIntent.getActivity( ctx, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    mBuilder.setContentIntent(resultPendingIntent);
+
+                    //now show the notification:
+                    int mNotificationId = 0634;
+                    NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    mNotifyMgr.notify(mNotificationId, mBuilder.build());
+                }
+
+                else if (adapterView.getItemAtPosition(i).equals("Auto GPS")) {
                     Uri gmmIntentUri = Uri.parse("geo:0,0?q=city+hall, Ottawa, ON");
                     Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
                     mapIntent.setPackage("com.google.android.apps.maps");
@@ -117,7 +143,50 @@ public class AutoListView extends AppCompatActivity {
                 }
             }
         });
+    }
 
+    public void updateTemp(long tempid, String temp) {
+        ContentValues values = new ContentValues();
+        values.put(AutoDatabaseHelper.KEY_TEMPERATURE, temp);
+        db.update(AutoDatabaseHelper.DATABASE_NAME, values, AutoDatabaseHelper.KEY_ID + "=" + tempid, null);
+        refreshDB();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        final Bundle bun = data.getExtras();
+        Long tempid = bun.getLong("id");
+        String temp = bun.getString("Temperature");
+        String toastText = bun.getString("Response");
+        if (requestCode==5 && resultCode == 0) {
+            updateTemp(tempid, temp);
+            Toast toast = Toast.makeText(AutoListView.this, toastText, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    private void refreshDB() {
+        dbHelper = new AutoDatabaseHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        results = db.query(false, AutoDatabaseHelper.DATABASE_NAME,
+                new String[] { AutoDatabaseHelper.KEY_ID, AutoDatabaseHelper.KEY_GPS_ENTRY, AutoDatabaseHelper.KEY_TEMPERATURE, AutoDatabaseHelper.KEY_NORMAL_SWITCH,
+                        AutoDatabaseHelper.KEY_HEAD_SWITCH, AutoDatabaseHelper.KEY_INSIDE_BRIGHTNESS, AutoDatabaseHelper.KEY_RADIO_VOLUME, AutoDatabaseHelper.KEY_RADIO_MUTE,
+                        AutoDatabaseHelper.KEY_RADIO_STATION_ONE, AutoDatabaseHelper.KEY_RADIO_STATION_TWO, AutoDatabaseHelper.KEY_RADIO_STATION_THREE, AutoDatabaseHelper.KEY_RADIO_STATION_FOUR,
+                        AutoDatabaseHelper.KEY_RADIO_STATION_FIVE, AutoDatabaseHelper.KEY_RADIO_STATION_SIX}, null, null, null, null, null, null);
+        int rows = results.getCount(); //number of rows returned
+        results.moveToFirst(); //move to first result
+    }
+
+    public void removeFragment() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.auto_FragmentHolder, frag).commit();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        dbHelper.close();
+        db.close();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 
     public boolean onCreateOptionsMenu(Menu m) { //put the menu in the toolbar
@@ -151,7 +220,7 @@ public class AutoListView extends AppCompatActivity {
             case R.id.auto_helpMenu:
                 Log.i("Navigation", "4");
                 AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
-                builder.setTitle("Welcome to Smart Home Auto Setting").setMessage("This is the Auto Setting part of the Smart Home App. You can use 'Add' button create your new component of a car. Version 1.0 by Zhen Qu.")
+                builder.setTitle("Welcome to Smart Home Auto Setting").setMessage("This is the Auto Setting part of the Smart Home App. You can set Temperature, Lights, Radio Stations and GPS of a car. Version 1.0 by Zhen Qu.")
                         .setNegativeButton("Ok", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
                                 Log.i("No", "No");
