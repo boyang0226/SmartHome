2614009121956d9fab7074b542b29e4298ce69df
Zhen <qu000010@algonquinlive.com>
2017-04-05 21:16:58 -0400

    Milestone 2 plus

--- app/src/main/java/com/example/bo/smarthome/AutoGPSFragment.java
+++ app/src/main/java/com/example/bo/smarthome/AutoGPSFragment.java
@@ -11,7 +11,6 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -58,7 +57,6 @@ public class AutoGPSFragment extends Fragment {
         super.onAttach(activity);
         ctx=activity;
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
--- app/src/main/java/com/example/bo/smarthome/AutoTemperatureFragment.java
+++ app/src/main/java/com/example/bo/smarthome/AutoTemperatureFragment.java
@@ -7,15 +7,12 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class AutoTemperatureFragment extends Fragment {
     String temp;
